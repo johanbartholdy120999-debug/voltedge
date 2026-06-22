926c69d639afb3317f76b897afc3263069f6fd79
johanbartholdy120999-debug <johanbartholdy120999@gmail.com>
2026-06-22 19:33:32 +0200

    Fix API test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
 git add .
 git commit -m "Add API tests"
 git push
+=======
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_health():
+    response = client.get("/")
+    assert response.status_code == 200
+>>>>>>> 418a559 (Fix API test)
