dfad6c5ff38f46febb11477eaf6e9c6f907be61a
johanbartholdy120999-debug <johanbartholdy120999@gmail.com>
2026-06-23 15:23:23 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/voltedge"
+DATABASE_URL = "sqlite:///./voltedge.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
