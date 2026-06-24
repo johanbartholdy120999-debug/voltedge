1640e9feb2b6169b612b392832f66de8b28e1d28
johanbartholdy120999-debug <johanbartholdy120999@gmail.com>
2026-06-24 22:28:32 +0200

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,14 +3,9 @@ from main import app
 
 client = TestClient(app)
 
-<<<<<<< HEAD
 def test_health():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json()["status"] == "running"
     
-=======
-def test_root():
-    response = client.get("/")
-    assert response.status_code == 200
->>>>>>> 31fc2640b81234a7acd0e181677c44420260ea4a
+
